cd2d5da6e67cf183b1b03f389258784a4ac8f082
Pavel <>
2018-10-24 14:46:45 +0400

    Optimized travis builds.

--- push.sh
+++ push.sh
@@ -1,6 +1,11 @@
 
 #!/bin/sh
 
+install_pandoc(){
+    sudo apt-get update
+    sudo apt-get install -y texlive-full texlive-xetex lmodern pandoc
+}
+
 setup_git() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
@@ -17,6 +22,10 @@ upload_files() {
     git push --quiet --set-upstream origin-pages doc-pages
 }
 
-setup_git
-commit_doc_files
-upload_files
+if [ "$TRAVIS_BRANCH" == "docs" ];then
+    install_pandoc
+    pandoc -f markdown --latex-engine=xelatex -V mainfont="DejaVu Sans Mono" -o docs/README.pdf README.md
+    setup_git
+    commit_doc_files
+    upload_files
+fi
