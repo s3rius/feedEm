2db60b28cb0cbf98e44b31e6dd5eb0d4986b6fd9
Pavel Kirilin <win10@list.ru>
2018-11-26 01:38:25 +0400

    Migrated to buefy. Description: * Added vue style library with full bulma support. * Added admins ui. * Added errorSnackbar component.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

---
+++ app/app/javascript/components/ErrorSnackBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="errors">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ErrorSnackBar",
+        props: {
+            errors: {
+                default: () => []
+            }
+        },
+        mounted: function () {
+            if (this.errors.length > 0) {
+                this.errors.forEach(err => {
+                    this.$snackbar.open({
+                        message: `Error found: ${err}`,
+                        type: 'is-danger',
+                        position: 'is-bottom-left'
+                    })
+                });
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- app/app/javascript/packs/application.js
+++ app/app/javascript/packs/application.js
@@ -11,16 +11,20 @@ console.log("Hello World from Webpacker");
 import TurbolinksAdapter from "vue-turbolinks";
 import Vue from "vue/dist/vue.esm";
 import App from "../components/app";
-import "bulma/css/bulma.css";
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import FeedEmNavBar from "../components/FeedemNavBar";
 import FeedemTitle from "../components/TitleComponent";
 import FeedemForm from "../components/AddFormComponent";
+import FeedemErrorSnack from "../components/ErrorSnackBar";
 
 Vue.use(TurbolinksAdapter);
+Vue.use(Buefy);
 Vue.component("app", App);
 Vue.component("f-navbar", FeedEmNavBar);
 Vue.component("f-title", FeedemTitle);
 Vue.component("f-addform", FeedemForm);
+Vue.component("f-error-snack", FeedemErrorSnack);
 
 document.addEventListener("turbolinks:load", () => {
     const mv = new Vue({
