010ed1e4e0d6bad4a0905b31c919e48fe86efa81
Pavel Kirilin <win10@list.ru>
2018-11-27 02:22:59 +0400

    Working on #17. Description: Added: * Welcome page with simple ui. Added components: * SearchBar * TitleWelcome Fixed: * Navigation bar links.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

---
+++ app/app/javascript/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="columns is-mobile is-centered">
+        <div class="column is-half is-narrow">
+            <div class="field has-addons">
+                <div class="control is-expanded">
+                    <input class="input" v-model="query" type="text"
+                           placeholder="Find food or Seller">
+                </div>
+                <div class="control">
+                    <a class="button is-info">
+                        Search
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SearchBar",
+        data: function () {
+            return {
+                query: ''
+            }
+        },
+        watch: {
+            query: function (val, old_val) {
+                console.log(`Val was ${old_val}. Became ${val}`)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'search_api/search'
+  get 'welcome/index'
   resources :merchandises
   resources :order_items
   resources :orders
@@ -7,5 +8,6 @@ Rails.application.routes.draw do
   resources :cards
   resources :admins
   resources :customers
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
