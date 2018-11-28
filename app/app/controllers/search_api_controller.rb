499d35c0dd0d29dd104f13d983ed44054568fb9e
Kolaer <overlordin777@gmail.com>
2018-11-28 20:55:05 +0400

    Fixed search API: nil query.
    Progress on #29.
    
    Signed-off-by: Kolaer <overlordin777@gmail.com>

--- app/app/controllers/search_api_controller.rb
+++ app/app/controllers/search_api_controller.rb
@@ -1,9 +1,9 @@
 class SearchApiController < ApplicationController
   def search
-    query_string = params[:query].strip
-    limit        = params[:limit].to_i
+    query = params[:query].to_s.strip
+    limit = params[:limit].to_i
 
-    if query_string.empty? then
+    if query.empty? then
       render :json => {:res => 'empty_query'}
       return
     end
@@ -14,13 +14,13 @@ class SearchApiController < ApplicationController
 
     result = []
 
-    sellers_names = Seller.search_by_name(query_string).map(&:name)
+    sellers_names = Seller.search_by_name(query).map(&:name)
 
     sellers_names.each { |name|
       result << {:suggest => name, :type => 'Seller'}
     }
 
-    merch_names = Merchandise.search_by_name(query_string).map(&:name)
+    merch_names = Merchandise.search_by_name(query).map(&:name)
 
     merch_names.each { |name|
       result << {:suggest => name, :type => 'Order item'}
