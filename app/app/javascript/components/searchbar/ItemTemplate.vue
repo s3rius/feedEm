157de5b419e480eb34f3bc912dd0a63e253f5681
Pavel Kirilin <win10@list.ru>
2018-12-04 00:43:02 +0400

    Added simple autosuggest. Description: * Added autosugest for SearchBar. * Fixed some style issues.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

---
+++ app/app/javascript/components/searchbar/ItemTemplate.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="searchItem">
+        <b>#{{item.id}}</b>
+        <span>{{ item.name }}</span>
+        <abbr>{{item.body}}</abbr>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            item: {required: true},
+            searchText: {required: true}
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .searchItem {
+        background-color: #fff;
+    }
+
+</style>
