08bf4436057a649f6afeaa68982419e897f9892f
Pavel Kirilin <win10@list.ru>
2018-12-05 00:03:27 +0400

    Fixed Search, Added seeds.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -7,26 +7,40 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-sellers = %w[CoolFood FakeBurgers FunnyCrap FilthyCream ShittyDonuts Sim BitchyMilk]
+sellers = %w[CoolFood
+             FakeBurgers
+             FunnyCrap
+             FilthyCream
+             ShittyDonuts
+             Sim
+             BitchyMilk]
 
 sellers.each do |name|
   seller = Seller.create(name: name)
   merchandises = [
-      %w[shit fuck 20],
-      %w[borgir meat 30],
-      %w[donut, crap, 10],
-      %w[Fluke, crap, 10],
-      %w[Fuckface, you, 1],
-      %w[cream, knif, 21]
+    %w[shit fuck 20],
+    %w[borgir fish 30],
+    %w[burgar beef 30],
+    %w[bregar rats 30],
+    %w[donut crap 10],
+    %w[Fluke shit 10],
+    %w[Fuckface you 1],
+    %w[cream gold 21],
+    %w[semen protein 3],
+    %w[bitchWax saliva 15]
   ]
   merchandises.each do |m_name, m_desc, m_price|
-    Merchandise.create(name: m_name, description: m_desc, price: m_price, seller: seller)
+    Merchandise.create(name: m_name,
+                       description: 'made from ' + m_desc,
+                       price: m_price,
+                       seller: seller)
   end
 end
 
 cu_users = [
-    %w[s3rius 19216211 Pavel Kirilin],
-    %w[kolaer 32883212 Maxim Naumov]
+  %w[s3rius 19216211 Pavel Kirilin],
+  %w[kolaer 32883212 Maxim Naumov],
+  %w[animeThug 12332122 Andrei Belousov]
 ]
 
 cu_users.each do |username, pass, name, surname|
