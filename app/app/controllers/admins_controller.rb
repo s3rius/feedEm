f95c73c6d7592c7cff537c6a6a1327a69ae56e5f
Pavel Kirilin <win10@list.ru>
2018-12-25 12:14:51 +0400

    Fixed buggy bugs.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- app/app/controllers/admins_controller.rb
+++ app/app/controllers/admins_controller.rb
@@ -70,6 +70,6 @@ class AdminsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admin_params
-      params.require(:admin).permit(:username, :password, :password_confirmation)
+      params.require(:admin).permit(:email, :password, :password_confirmation)
     end
 end
